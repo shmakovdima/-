2827a15f37dc2d932e6a24a613acee9f03cfa960
shmakovdima <contact@shmakovdima.ru>
2015-10-02 22:51:35 +0300

    футер

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -15,12 +15,12 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-4">
-					<span class="align-left">2015 Copyright VeroGusto Travel</span>
+					<span>2015 Copyright VeroGusto Travel</span>
 				</div>
 				<div class="col-sm-4 footer__logo"></div>
 				<div class="col-sm-4">
 					<ul>
-						<li></li>
+						<li><a href="https://vk.com/test" title="Мы в Вконтакте" class="fa-vk"></a></li>
 						<li></li>
 						<li></li>
 						<li></li>
@@ -30,7 +30,9 @@
 		</div>
 	</footer><!-- .site-footer -->
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.min.js"></script>
+    
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/bootstrap.min.js"></script>
 <?php wp_footer(); ?>
