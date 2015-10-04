542a767c1e263871837679b24638ad096905d62c
shmakovdima <contact@shmakovdima.ru>
2015-10-04 03:19:37 +0300

    to big pc 3

--- wp-content/themes/twentyfifteen/js/scripts.js
+++ wp-content/themes/twentyfifteen/js/scripts.js
@@ -1,7 +1,9 @@
-new WOW().init();
+
+
 
 // When the DOM is ready, run this function
 $(document).ready(function() {
+	new WOW().init();
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
